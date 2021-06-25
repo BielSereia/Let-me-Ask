733969248f075a5e532f2078bd6f6f09cf872942
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 16:56:33 -0300

    Quarta Aula

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createContext } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 import { Home } from "./pages/Home/index";
 import { NewRoom } from "./pages/NewRoom/index";
 import { Room } from "./pages/Room/index";
@@ -27,6 +28,8 @@ function App() {
           <Route path="/" exact={true} component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
